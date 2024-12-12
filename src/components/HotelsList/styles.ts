f1e05bf9bd56d91647c0a4ef4f1ab0795d1c8c36
ghazaryanrozi <roza.ghazaryan.9@gmail.com>
2024-12-12 21:18:31 +0400

    fixed padding

--- src/components/HotelsList/styles.ts
+++ src/components/HotelsList/styles.ts
@@ -2,7 +2,7 @@ import { Platform, StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
     hotelsListContainer: {
-        paddingTop: Platform.OS === 'ios' ? 64 : 0,
+        paddingTop: Platform.OS === 'ios' ? 64 : 12,
         paddingHorizontal: 16,
         paddingBottom: 108,
     },
